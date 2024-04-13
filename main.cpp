045cb91f1936eacefcee6efdafa242e62f1b81e7
Soham Kukreti <kukretisoham@gmail.com>
2024-04-13 22:52:17 +0530

    Add Chess pieces to board

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 using namespace std;
-#define WHITE_SQUARE 0xDB
-#define BLACK_SQUARE 0xFF
 
 class ChessBoard{
     public:
         ChessBoard(){
             for(int i = 0; i < 8; i++){
                 for(int j = 0; j < 8; j++){
-                    board[i][j] = '0';
+                    board[i][j] = ' ';
                 }
+            }
+            board[0][0] = 'R';
+            board[0][1] = 'N';
+            board[0][2] = 'B';
+            board[0][3] = 'Q';
+            board[0][4] = 'K';
+            board[0][5] = 'B';
+            board[0][6] = 'N';
+            board[0][7] = 'R';
+            for(int i = 0; i < 8; i++){
+                board[1][i] = 'P';
+            }
+            board[7][0] = 'r';
+            board[7][1] = 'n';
+            board[7][2] = 'b';
+            board[7][3] = 'q';
+            board[7][4] = 'k';
+            board[7][5] = 'b';
+            board[7][6] = 'n';
+            board[7][7] = 'r';
+            for(int i = 0; i < 8; i++){
+                board[6][i] = 'p';
             }
         };
         void printBoard(){
